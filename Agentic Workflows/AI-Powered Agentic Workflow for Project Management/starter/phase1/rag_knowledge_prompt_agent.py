4fa030cc6b263869efff2f1814e8eb5226a43b01
Paramvir Singh <veershgp@gmail.com>
2025-08-27 20:03:30 +1000

    Phase 2 completion

--- Agentic Workflows/AI-Powered Agentic Workflow for Project Management/starter/phase1/rag_knowledge_prompt_agent.py
+++ Agentic Workflows/AI-Powered Agentic Workflow for Project Management/starter/phase1/rag_knowledge_prompt_agent.py
@@ -1,15 +1,17 @@
 # TODO: 1 - Import RAGKnowledgePromptAgent
+from workflow_agents.base_agents import RAGKnowledgePromptAgent
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+load_dotenv(find_dotenv(usecwd=True), override=True)
 
-# Define the parameters for the agent
+api_key = os.getenv("OPENAI_API_KEY")
+assert api_key, "OPENAI_API_KEY not loaded from .env"
+
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 persona = "You are a college professor, your answer always starts with: Dear students,"
-RAG_knowledge_prompt_agent = # TODO: 2 - Instantiate RAGKnowledgePromptAgent
+RAG_knowledge_prompt_agent = RAGKnowledgePromptAgent(openai_api_key, persona) # TODO: 2 - Instantiate RAGKnowledgePromptAgent
 
 knowledge_text = """
 In the historic city of Boston, Clara, a marine biologist and science communicator, began each morning analyzing sonar data to track whale migration patterns along the Atlantic coast.
@@ -42,8 +44,10 @@ To Clara, knowledge was a living system—retrieved from the past, generated in 
 Her life and work were testaments to the power of connecting across disciplines, borders, and generations—exactly the kind of story that RAG models were born to find.
 """
 
-chunks = # TODO: 3 - Compute the knowledge_text the chunks
-embbedings = # TODO: 4 - Compute the knowledge_text the chunk embbedings
+chunks = RAG_knowledge_prompt_agent.chunk_text(knowledge_text) # TODO: 3 - Compute the knowledge_text the chunks
+embeddings_df = RAG_knowledge_prompt_agent.calculate_embeddings() # TODO: 4 - Compute the knowledge_text the chunk embbedings
 
 prompt = "What is the podcast that Clara hosts about?"
 # TODO: 5 - Print the prompt and the response
+response = RAG_knowledge_prompt_agent.find_prompt_in_knowledge(prompt)
+print("Response:", response)
